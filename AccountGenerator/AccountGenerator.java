b46a77dc3afa0218f46d9ff21ffbec3b4ca2c680
Allisyn Ypma <allisynypma@u.boisestate.edu>
2026-06-13 13:28:14 -0600

    LabWarmUp is Complete

---
+++ AccountGenerator/AccountGenerator.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class AccountGenerator 
+{
+    public static void main(String[] args)
+    {
+        Scanner kbd = new Scanner(System.in);
+        Random myGen = new Random();
+
+        /* Input processing */
+        System.out.print("Enter your first name: ");
+        String userFirstName = kbd.nextLine();
+        System.out.print("Enter your last name: ");
+        String userLastName = kbd.nextLine();
+
+        /* Convert user values to lower Case */
+        userFirstName = userFirstName.toLowerCase();
+        userLastName = userLastName.toLowerCase();
+
+        /* Grab specific charaters from user input */
+        char firstLetterOfFirstName = userFirstName.charAt(0);
+        String firstFiveOfLastName = userLastName.substring(0,5);
+
+        /* Grab random int */
+        int myInt = myGen.nextInt(90) + 10;
+
+        /* Output processing */
+        String username = firstLetterOfFirstName + firstFiveOfLastName + myInt;
+        System.out.print("Username: " + username);
+        
+        kbd.close();
+    }   
+}
